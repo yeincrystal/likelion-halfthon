40b4e1eaf13af7fd1d7087bc9f2a38ca8590a621
Yein <yeincrystal@gmail.com>
2020-10-18 00:52:40 +0900

    Create Hackathon Django Project

---
+++ resume/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+ #import views from this directory 
+
+urlpatterns = [
+    path('', views.index, name="index"),
+]
